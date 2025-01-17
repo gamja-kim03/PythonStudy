dceee84ea9eba0338273a2d54a03bd46198fa5d2
doyeon kim <kkdy0416@naver.com>
2025-01-17 15:49:48 +0900

    update

---
+++ First_week/four.py
@@ -0,0 +1,7 @@
+print('결과')
+a = int(input('input number : '))
+
+for i in range (a):
+    for j in range(i+1):
+        print('*', end=' ')
+    print('')
---
+++ First_week/three.py
@@ -0,0 +1,5 @@
+print('결과')
+ori = [2,6,5,3,6,9]
+
+print('Original Array : ',ori)
+print('New Array : ', ori[::-1])
